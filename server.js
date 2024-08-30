e0163da42e51529dcf5f746f3d1759d9597f79f6
DuRg3sH-8H <71206614+DuRg3sH-8H@users.noreply.github.com>
2024-08-30 22:41:36 +0545

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin:
+    "https://verification-client-git-main-durg3sh-8hs-projects.vercel.app/", 
+  methods: "GET,POST",
+  allowedHeaders: "Content-Type",
+};
+
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
 
-app.post('/verify', (req, res) => {
+app.post("/verify", (req, res) => {
   const { code } = req.body;
 
   if (!code || code.length !== 6 || isNaN(code)) {
-    return res.status(400).json({ error: 'Invalid code' });
+    return res.status(400).json({ error: "Invalid code" });
   }
 
-  if (code.endsWith('7')) {
-    return res.status(400).json({ error: 'Verification Error' });
+  if (code.endsWith("7")) {
+    return res.status(400).json({ error: "Verification Error" });
   }
 
-  return res.status(200).json({ message: 'Verification Successful' });
+  return res.status(200).json({ message: "Verification Successful" });
 });
 
 const PORT = process.env.PORT || 5000;
